45b6bfece61786a0c32ea1f28a6dbf9e1a030bf8
OwinoLucas <lucowish35@gmail.com>
2023-07-17 12:03:17 +0300

    UI updates

--- module/club/Module.js
+++ module/club/Module.js
@@ -3,7 +3,19 @@
 const Base = require('e-champ/module/club/Module');
 
 module.exports = class ClubModule extends Base {
-
+    static getConstants () {
+        return {
+            BEHAVIORS: {
+                'access': {
+                    Class: require('areto/filter/AccessControl'),
+                    rules: [{
+                        'allow': true,
+                        'permissions': ['@']
+                    }]
+                }
+            }
+        };
+    }
     constructor (config) {
         super({
             original: Base,
